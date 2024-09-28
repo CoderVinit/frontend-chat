0905bcc07453937757ee65bb5d7690c8007c4937
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2024-09-28 12:49:09 +0530

    Update adminAuth.js

--- src/redux/thunks/adminAuth.js
+++ src/redux/thunks/adminAuth.js
@@ -1,8 +1,6 @@
 
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
-// import { server } from '../../constants/Config'
-import { validate } from '../../../../server/lib/validators';
 
 
 const adminLogin = createAsyncThunk(

3468224a131bc75b68d8dce128865ccd2d7881c0
CoderVinit <vinitpatel8896@gmail.com>
2024-09-28 12:42:26 +0530

    dits

---
+++ dist/assets/AdminLogin-Zc5wKhwl.js
@@ -0,0 +1,1 @@
+import{j as a,a2 as t}from"./index-BRYrtOYy.js";import{u as o}from"./index-DdwGzNOV.js";const r=()=>(o(""),a.jsx(t,{to:"/admin/dashboard"}));export{r as default};
---
+++ dist/assets/NotFound-D_9E8dpX.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BRYrtOYy.js";const r=()=>o.jsx("div",{children:"Not found"});export{r as default};
